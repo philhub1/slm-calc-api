da92b906abffe5a2e6eece20ee359963575baa8a
philhub1 <wi22b017@technikum-wien.at>
2023-06-06 18:46:03 +0200

    initial commit

---
+++ src/test/java/at/technikum/slmcalcapi/SlmCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmcalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmCalcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
